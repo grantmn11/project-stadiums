d3952af624ea6094ac224897df9acc9bd73f8c41
grantmn11 <grantferreira11@gmail.com>
2020-10-14 18:33:30 -0700

    const and jQuery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('sanity check')
+// API Key and const variables
+const API_Key = '1';
+const BASE_URL = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=';
+
+let userInput;
+let stadiumData;
+
+const $stadiumPic = $('#stadiumPic');
+const $location = $('#location');
+const $website = $('#website');
+const $league = $('#league');
+const $year = $('#year');
+const $form = $('form');
+const $input = $('input [type="text"]');
+
+$form.on('submit', function())
+
+
+
