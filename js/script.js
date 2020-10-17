73ab97ebec1414c2055ea443043bef959568e958
Grant F <69698724+grantmn11@users.noreply.github.com>
2020-10-17 14:39:29 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // API Key and const variables
-const API_Key = "1";
+
 
 
 const BASE_URL = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=';
@@ -60,91 +60,5 @@ function render(){
 
 
 
-/*function generateUI(){
-    return pokemonData.results.map(function(pokemon){
-        return `
-        <article data-url="${pokemon.url}" class='card flex-ctr outline'>
-        <h3>${pokemon.name}</h3>
-        </article>`;
 
-/*
-// const and variables
-const API_KEY = '97483688dedf46a302f696b2b812060b';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
-//const redirect;
-//insert a link to ao new page or a picture for the error
-//cached elements references
-let userInput;
-let weatherData;
-
-const $temp = $('#temp');
-const $humidity = $('#humidity');
-const $pressure = $('#pressure');
-const $form = $('form');
-const $input = $('input[type="text"]');
-
-
-//event listeners
-$form.on('submit', handleGetData);
-
-
-//functions
-//q={city name}&appid={API key}'
-
-function handleGetData(evt){
-    evt.preventDefault(); //problem is here
-    userInput = $input.val();
-    console.log(userInput)
-    if(!userInput) return;
-    $.ajax(BASE_URL + userInput + '&units=metric&appid=' + API_KEY)
-    .then(function (data){
-        console.log(data)
-        weatherData = data;
-        render();
-        
-    }, function(error){
-        alert(`As a courtesy you have been redirected. Thank you for visitng. ${location.replace('https://www.weather.com')}`)
-    });
-}
-
-function render(){
-    $temp.text('The Temperature is: ' + weatherData.main.temp)
-    $humidity.text('UV index: ' + weatherData.main.humidity)
-
-}
-
- // add avariable to add an image then add an img src, then in render grab the element and set the attribute $img.attr('src', moviedata.poster)
- //$image $ id
- //render $ attr change src 
- // Constants and variables
-let movieData;
-let userInput;
-// Cached element references
-const $title = $('#title');
-const $year = $('#year');
-const $rating = $('#rating');
-const $form = $('form');
-const $input = $('input[type="text"]')
-// Event listeners
-$form.on('submit', handleGetData);
-// Functions
-function handleGetData(event) {
-    event.preventDefault(); // turn off default behavior (page refresh)
-    userInput = $input.val();
-    if(!userInput) return;
-    $.ajax('http://www.omdbapi.com/?apikey=5ceecba4&t=' + userInput) // use s parameter instead of t to get all info
-    .then(function(data) {
-        movieData = data;
-        render();
-    }, function(error) {
-        console.log('Error', error);
-    });
-}
-function render() {
-    $title.text('Movie Title: ' + movieData.Title);
-    $year.text('Year Released: ' + movieData.Year);
-    $rating.text('MPAA Rating: ' + movieData.Rated);
-}
-
-*/
 
